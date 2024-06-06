551ff38c7831b0a85a52f38c14ead7689bd177dd
Graham McLellan <graham@swordfoxdesign.co.nz>
2024-06-07 09:57:18 +1200

    Initial release

---
+++ README.md
@@ -0,0 +1,60 @@
+# Silverstripe CDN Rewrite Plus
+
+Provides a simple method of rewriting the URLs of assets and resources to allow the use of a subdomain or external CDN service
+
+See the configuration notes below for an example of how to set this module up.
+
+# Requirements
+*Silverstripe 4.x
+*Silverstripe 5.x
+
+# Installation
+* Install the code with `composer require swordfox/silverstripe-cdnrewrite-plus "^1"`
+* Run a `dev/build?flush` to update your project
+
+# Usage
+
+The module won't make any changes to your site unless you do a bit of configuration.  There are a few options you can set, done in a yml file:
+
+
+```yaml
+---
+Name: cdnconfig
+---
+
+Swordfox\CDNRewritePlus\CDNMiddleware:
+  cdn_rewrite: true
+  cdn_domain: 'https://cdn.example.com'
+  add_debug_headers: true
+  enable_in_dev: true
+  subdirectory: ''
+  add_prefetch: true
+  rewritelinks: true
+  rewritebgattr: true
+  assetprefix: 'site-'
+  rewrites:
+    - 'assets'
+    - 'site-resources'
+```
+
+The options are hopefully fairly self explanatory:
+
+* `cdn_rewrite` - globally enables and disables the module (default false - disabled)
+* `cdn_domain` - the full domain name of the CDN (required to enable module)
+* `add_debug_headers` - if enabled, adds extra HTML headers to show the various operations being applied (default false)
+* `enable_in_dev` - enable the CDN in dev mode (default false)
+* `subdirectory` - set this if your site is in a subdirectory (eg. for http://www.example.com/silverstripe - set this to 'silverstripe')
+* `add_prefetch` - set this to true if you want the module to automatically add a `<link rel="dns-prefetch">` tag to your html head to improve performance
+* `rewritelinks` - set this to false if your CDN does not support all file types e.g. PDFs
+* `rewritebgattr` - rewrites background-image: url(<url>) paths
+* `assetprefix` - adds a prefix to the assets directory for use on shared CDNS e.g. `https://cdn.example.com/site-assets/image.jpg`
+* `rewrites` - this is a list of the prefixes you wish to rewrite.  By default, CMS 4 exposes content in a _resources directory in the public structure, so you'll probably want that as a minimum.  You can add as many additional entires here as required.
+
+# Notes
+
+* The module is disabled in the CMS / admin system, so rewrites do not currently happen here
+* When enabled, the module will always add an HTTP header of `X-CDN: Enabled` to show that it's working, even if none of the other rewrite operations are carried out.  If this is not present and you think it should be, ensure that you have set `cdn_rewrite` to true, that you have specified the `cdn_domain` in your config file and that you have `enable_in_dev` set to true if you are testing in dev mode.
+
+
+# Credits
+* Complete copy of [DorsetDigital's silverstripe-cdnrewrite](https://github.com/DorsetDigital/silverstripe-cdnrewrite) with a few extra options.
---
+++ src/CDNMiddleware.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace Swordfox\CDNRewritePlus;
+
+use SilverStripe\Admin\AdminRootController;
+use SilverStripe\Control\Director;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\Middleware\HTTPMiddleware;
+use SilverStripe\Core\Config\Configurable;
+use SilverStripe\Core\Injector\Injectable;
+use SilverStripe\View\HTML;
+
+class CDNMiddleware implements HTTPMiddleware
+{
+
+    use Injectable;
+    use Configurable;
+
+    /**
+     * @config
+     *
+     * Enable rewriting
+     * @var bool
+     */
+    private static $cdn_rewrite = false;
+
+    /**
+     * @config
+     *
+     * The cdn domain incl. protocol
+     * @var string
+     */
+    private static $cdn_domain = '';
+
+    /**
+     * @config
+     *
+     * Enable rewrite in dev mode
+     * @var bool
+     */
+    private static $enable_in_dev = false;
+
+    /**
+     * @config
+     *
+     * Add debug headers for each operation
+     * @var bool
+     */
+    private static $add_debug_headers = false;
+
+    /**
+     * @config
+     *
+     * Subdirectory name for the site
+     * @var string
+     */
+    private static $subdirectory = '';
+
+    /**
+     * @config
+     *
+     * Add dns-prefetch links to the html head
+     * @var boolean
+     */
+    private static $add_prefetch = false;
+
+    /**
+     * @config
+     *
+     * Array of prefixes we wish to rewrite
+     * @var array
+     */
+    private static $rewrites = [];
+
+    /**
+     * Process the request
+     * @param HTTPRequest $request
+     * @param $delegate
+     * @return
+     */
+    public function process(HTTPRequest $request, callable $delegate)
+    {
+        $response = $delegate($request);
+
+        if (($this->canRun() === true) && ($response !== null)) {
+            $response->addHeader('X-CDN-Rewrites', 'Enabled');
+
+            if ($this->getIsAdmin($request) === false) {
+                $body = $response->getBody();
+                $this->rewriteTags($body);
+                $this->addPrefetch($body, $response);
+                $response->setBody($body);
+            }
+
+            if ($this->config()->get('add_debug_headers') == true) {
+                $response->addHeader('X-CDN-Domain', $this->config()->get('cdn_domain'));
+                $response->addHeader('X-CDN-Dir', $this->getSubdirectory());
+            }
+
+            if ($this->config()->get('cdn_rewrite') === true) {
+                $response->addHeader('X-CDN-Module', 'Active');
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * Check if we're OK to execute
+     * @return bool
+     */
+    private function canRun()
+    {
+        $confEnabled = $this->config()->get('cdn_rewrite');
+        $devEnabled = ((!Director::isDev()) || ($this->config()->get('enable_in_dev')));
+        return ($confEnabled && $devEnabled);
+    }
+
+
+    /**
+     * Rewrite all the tags we need
+     * @param $body
+     * @param $response
+     */
+    private function rewriteTags(&$body)
+    {
+        $body = ($body ?: '');
+
+        $cdn = $this->config()->get('cdn_domain');
+        $subDir = $this->getSubdirectory();
+        $assetprefix = $this->config()->get('assetprefix');
+        $rewrites = $this->config()->get('rewrites');
+        $rewritelinks = $this->config()->get('rewritelinks');
+        $rewritebgattr = $this->config()->get('rewritebgattr');
+
+        foreach ($rewrites as $rewrite) {
+            $cleanrewrite = trim($rewrite, '/');
+
+            $search = [
+                'src="' . $subDir . $cleanrewrite . '/',
+                'src="/' . $subDir . $cleanrewrite . '/',
+                'src=\"/' . $subDir . $cleanrewrite . '/',
+                Director::absoluteBaseURL() . $cleanrewrite . '/'
+            ];
+
+            if ($cleanrewrite == 'assets' and $assetprefix) {
+                $cleanreplace = $assetprefix . $cleanrewrite;
+            } else {
+                $cleanreplace = $cleanrewrite;
+            }
+
+            $replace = [
+                'src="' . $cdn . '/' . $subDir  . $cleanreplace . '/',
+                'src="' . $cdn . '/' . $subDir . $cleanreplace . '/',
+                'src=\"' . $cdn . '/' . $subDir  . $cleanreplace . '/',
+                $cdn . '/' . $subDir . $cleanreplace . '/'
+            ];
+
+            if (($cleanrewrite == 'assets' and $rewritelinks) or $cleanrewrite != 'assets') {
+                array_push($search, 'href="/' . $subDir . $cleanrewrite . '/');
+                array_push($replace, 'href="' . $cdn . '/' . $subDir . $cleanreplace . '/');
+            }
+
+            if ($rewritebgattr) {
+                array_push($search, 'background-image: url(/' . $subDir . $cleanrewrite . '/');
+                array_push($replace, 'background-image: url(' . $cdn . '/' . $subDir . $cleanrewrite . '/');
+            }
+
+            $body = str_replace($search, $replace, $body);
+        }
+    }
+
+    private function addPrefetch(&$body, &$response)
+    {
+        $body = ($body ?: '');
+
+        if ($this->config()->get('add_prefetch') === true) {
+            $prefetchTag = $this->getPrefetchTag();
+            $body = str_replace('<head>', "<head>" . $prefetchTag, $body);
+            if ($this->config()->get('add_debug_headers') == true) {
+                $response->addHeader('X-CDN-Prefetch', 'Enabled');
+            }
+        }
+    }
+
+    private function getSubdirectory()
+    {
+        $subDir = trim($this->config()->get('subdirectory'), '/');
+        if ($subDir != "") {
+            $subDir = $subDir . '/';
+        }
+        return $subDir;
+    }
+
+    private function getPrefetchTag()
+    {
+        $atts = [
+            'rel' => 'dns-prefetch',
+            'href' => $this->config()->get('cdn_domain')
+        ];
+        $pfTag = "\n" . HTML::createTag('link', $atts);
+
+        return $pfTag;
+    }
+
+    /**
+     * Determine whether the website is being viewed from an admin protected area or not
+     * (shamelessly based on https://github.com/silverstripe/silverstripe-subsites)
+     *
+     * @param HTTPRequest $request
+     * @return bool
+     */
+    private function getIsAdmin(HTTPRequest $request)
+    {
+        $adminPath = AdminRootController::admin_url();
+        $currentPath = rtrim($request->getURL(), '/') . '/';
+        if (substr($currentPath, 0, strlen($adminPath)) === $adminPath) {
+            return true;
+        }
+        return false;
+    }
+}
